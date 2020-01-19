158947d8651573dc3727c580185b28fa5ef83ef1
Anthony Dees <spirawebdev@gmail.com>
2020-01-18 21:31:50 -0700

    BUtton functionality was added! You can now create your own buttons, but for some reason the path to the giphy api is completely non-functioning.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,53 +2,46 @@
 $(document).ready(function () {
     
     //Api Key and Stuff
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=2T0N6Y12oTEnnTOf44fmMkIx0eexxc16&limit=10");
-    xhr.done(function(response) { console.log("success got data", response); });
-    // Example queryURL for Giphy API
-    
- 
+    //var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=2T0N6Y12oTEnnTOf44fmMkIx0eexxc16&limit=10");
+   
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=2T0N6Y12oTEnnTOf44fmMkIx0eexxc16&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
     
     // Create array named topics
     var topics = ['tea', 'coffee', 'sweets', 'jazz', 'rain']
     // Create a for loop that creates a button for every value in the array
     for (var i = 0; i < topics.length; i++) { 
-        var buttons = $("<button type=" + "button" + ">" + topics[i] + "</button>")
+        var buttons = $("<button type=" + "'button' " + "class='boot'" + ">" + topics[i] + "</button>")
         $("#buttonContainer").append(buttons); 
         console.log(topics[i]);
     }; 
 
-
-
 // create an onclick function that grabs images from the GIPHY api
-    $('button').on('click', function() {
-       var jiffs = response;
-       jiffs;
-        console.log($(this).text())
-    });
+// $('button').on('click', function() {
+//     console.log("moop");
+//     var mlep =  $('.potato').append("<img src=" + queryURL[0].images.downsized_large + "/>" );
+       
+//    });
+       
+// Put search info back into for loop
+$("#searchButton").on('click', function() {
+    var searchValue = $('#searchBar').val();
+    topics.push(searchValue);
+    console.log(topics);
+    var buttons = $("<button type=" + "'button' " + "class='boot'" + ">" + searchValue + "</button>")
+        $("#buttonContainer").append(buttons); 
 
-
-
-
-
-    // Search button will create more buttons to click
-
-    $('#searchButton').on('click', function() {
-        search = $('#searchBar').val().trim();
-        console.log(search)
-        alert('potato')
-    });
 
     
 
-//Somehow pause gif ( I have absolutely no clue how to do this)
-
-
-
-
-
-
-
-
-
+    
+});
 
 });
+
