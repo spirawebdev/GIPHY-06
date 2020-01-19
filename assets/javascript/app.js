20280faede1bf65a0df377ffa3dba2412ab0e413
Anthony Dees <spirawebdev@gmail.com>
2020-01-18 23:17:19 -0700

    Final Working version, but for some reason there's a special google chrome incompatability that I can't fix. So the gifs wont load, but they will appear.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,19 @@
 $(document).ready(function () {
     
     //Api Key and Stuff
-    //var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=2T0N6Y12oTEnnTOf44fmMkIx0eexxc16&limit=10");
    
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=2T0N6Y12oTEnnTOf44fmMkIx0eexxc16&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=2T0N6Y12oTEnnTOf44fmMkIx0eexxc16&limit=10/>";
 
     $.ajax({
       url: queryURL,
       method: "GET"
+
     }).then(function(response) {
       console.log(response);
     });
     
     // Create array named topics
-    var topics = ['tea', 'coffee', 'sweets', 'jazz', 'rain']
+    var topics = ['tea', 'coffee', 'sweets', 'jazz', 'rain','boba', 'study', 'cafe', 'fog', 'cake']
     // Create a for loop that creates a button for every value in the array
     for (var i = 0; i < topics.length; i++) { 
         var buttons = $("<button type=" + "'button' " + "class='boot'" + ">" + topics[i] + "</button>")
@@ -22,14 +22,8 @@ $(document).ready(function () {
         console.log(topics[i]);
     }; 
 
-// create an onclick function that grabs images from the GIPHY api
-// $('button').on('click', function() {
-//     console.log("moop");
-//     var mlep =  $('.potato').append("<img src=" + queryURL[0].images.downsized_large + "/>" );
-       
-//    });
-       
-// Put search info back into for loop
+
+    // Put search info back into for loop
 $("#searchButton").on('click', function() {
     var searchValue = $('#searchBar').val();
     topics.push(searchValue);
@@ -38,10 +32,18 @@ $("#searchButton").on('click', function() {
         $("#buttonContainer").append(buttons); 
 
 
-    
 
     
 });
+
+// create an onclick function that grabs images from the GIPHY api
+ $('#buttonContainer').on('click', function() {
+     console.log("moop");
+     var mlep =  $('.display').append("<img src=" + 'http://api.giphy.com/v1/gifs/search?q=' + topics.this + "&api_key=2T0N6Y12oTEnnTOf44fmMkIx0eexxc16&limit=10/>");
+   mlep;
+    });
+       
+
 
 });
 
